fa1b5e65c2545a001240510a584e46a3de9aea73
damienbod <damien_bod@hotmail.com>
2023-08-06 10:30:23 +0200

    update dpop client

--- identityserver/Config.cs
+++ identityserver/Config.cs
@@ -14,39 +14,25 @@ public static class Config
     public static IEnumerable<ApiScope> ApiScopes =>
         new ApiScope[]
         {
-            new ApiScope("scope1"),
-            new ApiScope("scope2"),
+            new ApiScope("scope-dpop")
         };
 
     public static IEnumerable<Client> Clients =>
         new Client[]
         {
-            // m2m client credentials flow client
             new Client
             {
-                ClientId = "m2m.client",
-                ClientName = "Client Credentials Client",
+                ClientId = "web-dpop",
+                ClientSecrets = { new Secret("ddede2321s+90jjtmDc3/HiNmtKwuBZG9g6hnkqE=ku") },
 
-                AllowedGrantTypes = GrantTypes.ClientCredentials,
-                ClientSecrets = { new Secret("511536EF-F270-4058-80CA-1C89C192F69A".Sha256()) },
+                AllowedGrantTypes = GrantTypes.CodeAndClientCredentials,
 
-                AllowedScopes = { "scope1" }
-            },
-
-            // interactive client using code flow + pkce
-            new Client
-            {
-                ClientId = "interactive",
-                ClientSecrets = { new Secret("49C1A7E1-0C79-4A89-A3D6-A37998FB86B0".Sha256()) },
-
-                AllowedGrantTypes = GrantTypes.Code,
-
-                RedirectUris = { "https://localhost:44300/signin-oidc" },
-                FrontChannelLogoutUri = "https://localhost:44300/signout-oidc",
-                PostLogoutRedirectUris = { "https://localhost:44300/signout-callback-oidc" },
+                RedirectUris = { "https://localhost:5001/signin-oidc" },
+                FrontChannelLogoutUri = "https://localhost:5001/signout-oidc",
+                PostLogoutRedirectUris = { "https://localhost:5001/signout-callback-oidc" },
 
                 AllowOfflineAccess = true,
-                AllowedScopes = { "openid", "profile", "scope2" }
-            },
+                AllowedScopes = { "openid", "profile", "scope-dpop" }
+            }
         };
 }
