8470140cca734193fc7d9b87fe64142d59bfbba5
damienbod <damien_bod@hotmail.com>
2025-03-21 20:05:03 +0100

    clean up

--- identityserver/Config.cs
+++ identityserver/Config.cs
@@ -5,21 +5,18 @@ namespace IdentityServer;
 public static class Config
 {
     public static IEnumerable<IdentityResource> IdentityResources =>
-        new IdentityResource[]
-        {
+        [
             new IdentityResources.OpenId(),
             new IdentityResources.Profile(),
-        };
+        ];
 
     public static IEnumerable<ApiScope> ApiScopes =>
-        new ApiScope[]
-        {
+        [
             new ApiScope("scope-dpop")
-        };
+        ];
 
     public static IEnumerable<Client> Clients =>
-        new Client[]
-        {
+        [
             new Client
             {
                 ClientId = "web-dpop",
@@ -34,5 +31,5 @@ public static class Config
                 AllowOfflineAccess = true,
                 AllowedScopes = { "openid", "profile", "scope-dpop" }
             }
-        };
+        ];
 }
